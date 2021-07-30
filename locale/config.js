ed2630ff023f53b59749fa19b2cce629abfdd1c2
akoneko <akoneko47@gmail.com>
2021-07-30 20:39:06 +0700

    Update

--- locale/config.js
+++ locale/config.js
@@ -1,6 +1,35 @@
 // source: https://unpkg.com/dayjs@v1.10.4/locale/xx.js
 // see: https://github.com/iamkun/dayjs/blob/v1.10.4/src/locale/xx.js
 
-// TODO: copy compiled translations from https://unpkg.com/dayjs@v1.10.4/locale/xx.js
-
-dayjs.locale('xx');
+dayjs.locale({
+  name: 'th',
+  weekdays: 'อาทิตย์_จันทร์_อังคาร_พุธ_พฤหัสบดี_ศุกร์_เสาร์'.split('_'),
+  weekdaysShort: 'อาทิตย์_จันทร์_อังคาร_พุธ_พฤหัส_ศุกร์_เสาร์'.split('_'),
+  weekdaysMin: 'อา._จ._อ._พ._พฤ._ศ._ส.'.split('_'),
+  months: 'มกราคม_กุมภาพันธ์_มีนาคม_เมษายน_พฤษภาคม_มิถุนายน_กรกฎาคม_สิงหาคม_กันยายน_ตุลาคม_พฤศจิกายน_ธันวาคม'.split('_'),
+  monthsShort: 'ม.ค._ก.พ._มี.ค._เม.ย._พ.ค._มิ.ย._ก.ค._ส.ค._ก.ย._ต.ค._พ.ย._ธ.ค.'.split('_'),
+  formats: {
+    LT: 'H:mm',
+    LTS: 'H:mm:ss',
+    L: 'DD/MM/YYYY',
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY เวลา H:mm',
+    LLLL: 'วันddddที่ D MMMM YYYY เวลา H:mm'
+  },
+  relativeTime: {
+    future: 'อีก %s',
+    past: '%sที่แล้ว',
+    s: 'ไม่กี่วินาที',
+    m: '1 นาที',
+    mm: '%d นาที',
+    h: '1 ชั่วโมง',
+    hh: '%d ชั่วโมง',
+    d: '1 วัน',
+    dd: '%d วัน',
+    M: '1 เดือน',
+    MM: '%d เดือน',
+    y: '1 ปี',
+    yy: '%d ปี'
+  },
+  ordinal: n => `${n}.`
+}, null, false);
